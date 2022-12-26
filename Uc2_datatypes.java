65c50047fe1bb7a3a684340606b70a81fae0131a
PAVAN903 <navap903@gmail.com>
2022-12-26 14:54:34 +0530

    adding data types

---
+++ Uc2_datatypes.java
@@ -0,0 +1,21 @@
+package com.day1practice;
+
+public class Uc2_datatypes {
+	static int val1;
+	static double val2;
+	static float val3;
+	static boolean val4;
+	static long val5;
+	static String val6;
+
+	public static void main(String[] args) {
+		System.out.println("default values of primitive data types....");
+		System.out.println("value 1::" +val1);
+		System.out.println("value 2::" +val2);
+		System.out.println("value 3::" +val3);
+		System.out.println("value 4::" +val4);
+		System.out.println("value 5::" +val5);
+		System.out.println("value 6::" +val6);
+	}
+
+}
